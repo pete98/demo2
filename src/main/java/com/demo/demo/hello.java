353b8911710ae3d265d07301b9ebe65eece433df
pranavmac <pranavsailor@gmail.com>
2025-10-01 02:55:36 -0400

    changed java version

--- src/main/java/com/demo/demo/hello.java
+++ src/main/java/com/demo/demo/hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class hello {
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String hello() {
         return "Hello World!";
     }
